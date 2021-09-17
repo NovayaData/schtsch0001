bd84838eb8c5d9cc663ec1276ca8c0f1d232a059
Artem Schennikov <35872058+Schtschennikow@users.noreply.github.com>
2021-09-18 01:28:48 +0300

    expl

--- docs/YeastDiagram0002/main.js
+++ docs/YeastDiagram0002/main.js
@@ -17,6 +17,12 @@ const stars = {
     "5.5": "*****",
 };
 
+const expl = {
+    "vist_rank": "Число выступлений на заседаниях, их длительность в минутах, а также артистизм — восклицания, реакции публики",
+    "golos_rank": "Присутствие на решающих голосованиях",
+    "zakon_rank": "Число внесенных законопроектов и доля принятых"
+};
+
 var W = container.node().getBoundingClientRect().width,
     H = container.node().getBoundingClientRect().height;
 
@@ -34,66 +40,28 @@ const svg = container
 
 var points = ["5", "4", "3", "2", "1"]
 
-// function centroid(nodes) {
-//     let x = 0;
-//     let y = 0;
-//     let z = 0;
-//     for (const d of nodes) {
-//         let k = (d.times * 0.9 + 4) ** 2;
-//         x += d.x * k;
-//         y += d.y * k;
-//         z += k;
-//     }
-//     return {x: x / z, y: y / z};
-// };
-
-// function forceCluster() {
-//     const strength = 0.2;
-//     let nodes;
-
-//     console.log( centroid );
-
-//     function force(alpha) {
-
-//         const centroids = d3.rollup(nodes, centroid, d => {
-//             return d.party_name_clean;
-//         });
-//         const l = alpha * strength;
-
-//         for (const d of nodes) {
-//             // console.log(centroids.get(d.party_name_clean));
-//             const {x: cx, y: cy} = centroids.get(d.party_name_clean);
-//             d.vx -= (d.x - cx) * l;
-//             d.vy -= (d.y - cy) * l;
-//         }
-//     }
-
-//     force.initialize = _ => nodes = _;
-
-//     return force;
-// };
-
-var centerScale = d3.scalePoint().domain( points ).range([0, height-H*0.1]).padding(3),
+var centerScale = d3.scalePoint()
+    .domain( points )
+    .range([H*0.1, height-H*0.15])
+    .padding(3),
     forceStrength = 0.5;
 
 var simulation = d3.forceSimulation()
     .force("collide", d3.forceCollide(d => { return d.times * 0.9 + 4; }))
     .force("y", d3.forceY().y( d => { return centerScale(d.allIn_rank); } ).strength(0.1))
     .force("x", d3.forceX().x( width / 2 ).strength(0.4))
-    // .force('center', d3.forceY().y( d => { return centerScale(d.allIn_rank); } ))
     .force("charge", d3.forceManyBody() );
-    // .force("cluster", forceCluster());
 
-function addLeg(pos, text) {
-    svg.append("text")
-        .attr("y", d => {
-            return centerScale(pos);
-        })
-        .attr("x", width / 2)
-        .attr("text-anchor", "middle")
-        .attr("alignment-baseline", "middle")
-        .text(text)
-};
+// function addLeg(pos, text) {
+//     svg.append("text")
+//         .attr("y", d => {
+//             return centerScale(pos);
+//         })
+//         .attr("x", width / 2)
+//         .attr("text-anchor", "middle")
+//         .attr("alignment-baseline", "middle")
+//         .text(text)
+// };
 
 // addLeg("1", "*");
 // addLeg("2", "**");
@@ -150,10 +118,7 @@ d3.csv("data.csv").then( function(data){
                 var radius = d.times * 0.9 + 3;
                 return d.y = Math.max(radius, Math.min(height - radius, d.y)); 
             });
-
-            // .attr("cx", function(d){ return d.x; })
-            // .attr("cy", function(d){ return d.y; });
-    }
+    };
 
     simulation
         .nodes(data)
@@ -165,12 +130,7 @@ d3.csv("data.csv").then( function(data){
             return centerScale( d[byVar] );
         }));
 
-        // simulation.force('center', d3.forceY().strength(forceStrength).center( d => {
-        //      return centerScale( d[byVar] );
-        // }));
-
-        simulation.alpha(a).restart()
-        // simulation.force("charge", d3.forceManyBody(1));
+        simulation.alpha(a).restart();
     }
 
     function setupButtons() {
@@ -181,6 +141,8 @@ d3.csv("data.csv").then( function(data){
 
                 button.classed('active', true);
                 var buttonId = button.attr('id');
+
+                document.getElementById("expl").textContent = expl[buttonId];
 
                 splitBubbles(buttonId, 0.5);
         });
@@ -197,6 +159,7 @@ d3.csv("data.csv").then( function(data){
                         document.getElementById("lol").style.display = 'none';
                         document.getElementById("toolbar").style.display = 'inline';
                         document.getElementById("legend").style.display = 'inline';
+                        document.getElementById("expl").style.display = 'inline';
                     }, 2500
                 )
         });
@@ -209,36 +172,4 @@ d3.csv("data.csv").then( function(data){
             splitBubbles("allIn_rank", 2);
         }, 500
     );
-
-    // function get_start_pos() {
-    //     var start_pos = [];
-
-    //     allc = d3.selectAll("circle");
-
-    //     allc.nodes().forEach(
-    //         d => {
-
-    //             var bbd = d.getBBox();
-
-    //             var x = bbd.x,
-    //                 y = bbd.y,
-    //                 cid = d.id,
-    //                 cclass = d.getAttribute("class");
-                
-    //             var gh = (y - centerScale(cclass)) / (100 / width),
-    //                 gw = (x - width / 2) / (100 / height);
-                
-    //             starts = {
-    //                 "deputy_id" : cid.replace("c", ""),
-    //                 "start_y" : gh,
-    //                 "start_x" : gw
-    //             };
-
-    //             start_pos.push(starts);
-    //         }
-    //     );
-
-    //     console.log(JSON.stringify(start_pos));
-    // };
-
 });
